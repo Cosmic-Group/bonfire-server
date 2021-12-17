9b1122d97099f8ce16219ed46ee05101aee3f025
Giovani Vrech <giovanivrech@gmail.com>
2021-12-17 02:45:43 -0300

    refactor: connect to postgres before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,37 @@
+import { IBackup, IMemoryDb, newDb } from 'pg-mem'
 import request from 'supertest'
+import { getConnection } from 'typeorm'
+import { PgUser } from '../../infra/postgres/entities'
+
 import app from '../config/app'
 
+const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
+  const db = newDb()
+  const connection = await db.adapters.createTypeormConnection({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+  })
+
+  await connection.synchronize()
+  return db
+}
+
 describe('SignUp Middleware', () => {
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser])
+    backup = db.backup()
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
