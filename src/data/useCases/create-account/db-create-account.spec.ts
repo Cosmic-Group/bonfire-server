c446646d7be24e0c9cdf662f1b0e7cb9277e4d97
Giovani Vrech <giovanivrech@gmail.com>
2021-12-16 23:19:38 -0300

    test: ensure DBCreateAccount throws if Encrypter throws

--- src/data/useCases/create-account/db-create-account.spec.ts
+++ src/data/useCases/create-account/db-create-account.spec.ts
@@ -38,4 +38,18 @@ describe('DBCreateAccount UseCase', () => {
     await sut.create(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    const promise = sut.create(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
