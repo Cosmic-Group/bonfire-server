8eaf302e98a88752acf6162b5e5028093ce75973
Giovani Vrech <giovanivrech@gmail.com>
2021-12-23 16:27:49 -0300

    feat: add swagger API

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Bonfire API',
+    description: 'Bonfire is a communication application, with voice spaces, chat channels and screen sharing.',
+    version: '1.0.0'
+  }
+}
