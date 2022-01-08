2d73b198d001a0cbfe7de4736e1a937c78b0c53b
Giovani Vrech <giovanivrech@gmail.com>
2022-01-08 14:07:26 -0300

    test: ensure JwtAdapter throws if sign throw

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -22,4 +22,15 @@ describe('JWT Adapter', () => {
 
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_id')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
