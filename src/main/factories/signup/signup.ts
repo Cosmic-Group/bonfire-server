b12d02d3f7a83e26c5be1e70dcc39bad09318605
Giovani Vrech <giovanivrech@gmail.com>
2022-01-08 13:53:04 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,5 +1,5 @@
 import { DBCreateAccount } from '../../../data/useCases/create-account/db-create-account'
-import { BCrypterAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BCrypterAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { PgUserAccountRepository, PgLogErrorsRepository } from '../../../infra/postgres/repos'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { Controller } from '../../../presentation/protocols'
