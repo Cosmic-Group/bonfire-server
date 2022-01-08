c28cfbf0b5827589bc2c7be025d431adf8ae9f3b
Giovani Vrech <giovanivrech@gmail.com>
2022-01-08 16:06:49 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,23 +1,26 @@
+import * as dotenv from 'dotenv'
+
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
 import { Controller } from '../../../presentation/protocols'
 import { DbAuthentication } from '../../../data/useCases/authentication/db-authentication'
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
-import { PgLogErrorsRepository } from '../../../infra/postgres/repos'
-import { PgUserAccountRepository } from '../../../infra/postgres/repos/user-account-repository/user-account'
+import { PgUserAccountRepository, PgLogErrorsRepository } from '../../../infra/postgres/repos'
 import { BCrypterAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
+dotenv.config()
+
 export const makeLoginController = (): Controller => {
   const salt = 12
-  const bCrypterAdapter = new BCrypterAdapter(salt)
+  const bcryptAdapter = new BCrypterAdapter(salt)
 
   const jwtAdapter = new JwtAdapter(process.env.JWT_SECRET)
 
   const pgUserAccountRepository = new PgUserAccountRepository()
-  const dbAuthentication = new DbAuthentication(pgUserAccountRepository, bCrypterAdapter, jwtAdapter, pgUserAccountRepository)
+  const authentication = new DbAuthentication(pgUserAccountRepository, bcryptAdapter, jwtAdapter, pgUserAccountRepository)
 
-  const loginControler = new LoginController(dbAuthentication, makeLoginValidation())
+  const loginControler = new LoginController(authentication, makeLoginValidation())
 
   const pgLogErrorsRepository = new PgLogErrorsRepository()
   return new LogControllerDecorator(loginControler, pgLogErrorsRepository)
