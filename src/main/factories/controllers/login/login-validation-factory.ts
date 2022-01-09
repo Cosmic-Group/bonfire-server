87dbc7bd09e169037ba50962571352773f2c836f
Giovani Vrech <giovanivrech@gmail.com>
2022-01-09 02:23:37 -0300

    refactor: move validation to its own layer

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,7 +1,7 @@
 
-import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/presentation/controllers/login/login-controller-protocols.ts
+++ src/presentation/controllers/login/login-controller-protocols.ts
@@ -1,3 +1,2 @@
 export * from '../../protocols'
 export * from '../../../domain/useCases/authentication'
-export * from '../../protocols/validation'
