ccd0fd64f9a3c7886aff6cea3219ce4f64024495
Giovani Vrech <giovanivrech@gmail.com>
2022-01-10 20:46:35 -0300

    fix: make controllers return the error message instead of the error name

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -9,7 +9,7 @@ export const adaptRoute = (controller: Controller) => {
 
     const httpResponse = await controller.handle(httpRequest)
 
-    if (httpResponse.statusCode === 201 || httpResponse.statusCode === 200) {
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
       res.status(httpResponse.statusCode).json({
